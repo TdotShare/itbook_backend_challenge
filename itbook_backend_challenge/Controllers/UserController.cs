dc3188c6af47b9a036f1d7330d1cc7d78bee3609
Jirayu Ch <52649379+TdotShare@users.noreply.github.com>
2022-10-19 08:29:18 +0700

    Initial commit

---
+++ itbook_backend_challenge/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace itbook_backend_challenge.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet("user") ]
+        public String Index()
+        {
+            return "test !";
+        }
+    }
+}
